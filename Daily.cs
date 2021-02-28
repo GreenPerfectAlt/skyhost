931ed646096cc1b967c4f8b14ddcd33b02aed0f4
greenperfectalt@outlook.com <greenperfectalt@outlook.com>
2021-02-28 19:03:15 +0300

    началоДобавьте файлы проекта.

---
+++ Daily.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skyhost
+{
+    class Daily
+    {
+
+        public int id { get; set; }
+        public string time;
+        public Daily() { }
+
+        public Daily(int id, string time){
+            this.id = id;
+            this.time = time;
+        }
+    }
+}
